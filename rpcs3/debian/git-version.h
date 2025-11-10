b3e692279eec0a9cd4f4257ed6fe7586ac180c35
tohurtv <joshwebb84@outlook.com>
2025-11-09 20:38:35 -0700

    Add some more packages to the RakuOS repos

---
+++ rpcs3/debian/git-version.h
@@ -0,0 +1,10 @@
+// This is a generated file.
+
+#define RPCS3_GIT_VERSION "git~20251106"
+#define RPCS3_GIT_BRANCH "master"
+#define RPCS3_GIT_FULL_BRANCH "RPCS3/rpcs3/master"
+
+// If you don't want this file to update/recompile, change to 1.
+
+#define RPCS3_GIT_VERSION_NO_UPDATE 1
+
